2003807133c386f0bfb0f6d895e2675d568154e2
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-10-20 09:53:19 +0530

    cluster added for load balancing

--- api/app.js
+++ api/app.js
@@ -1,12 +1,17 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import cors from 'cors'
-import authRoute from './routes/auth.js'
-import userRoute from './routes/user.js'
-import taskRoute from './routes/task.js'
+import cors from "cors";
+import authRoute from "./routes/auth.js";
+import userRoute from "./routes/user.js";
+import taskRoute from "./routes/task.js";
 import cookieParser from "cookie-parser";
 
+import cluster from "node:cluster";
+import cpus from "node:os";
+const numCpus = cpus.availableParallelism();
+import process from "node:process";
 
 //* Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -14,51 +19,60 @@ process.on("uncaughtException", (err) => {
   console.log(`Shutting down the server due to Uncaught Exception`);
   process.exit(1);
 });
- 
-dotenv.config();
 
-const app = express();
-const port = process.env.PORT;
+//* Cluster
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+  // Fork workers.
+  for (let i = 0; i < numCpus; i++) {
+    cluster.fork();
+  }
 
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  const app = express();
+  const port = process.env.PORT;
 
+  // ? routes
 
-// ? routes
+  app.use(cors());
+  app.use(express.json());
+  app.use(cookieParser());
 
-app.use(cors())
-app.use(express.json())
-app.use(cookieParser())
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "conected" });
+  });
+  app.use("/api/auth", authRoute);
+  app.use("/api/user", userRoute);
+  app.use("/api/task", taskRoute);
 
-app.get("/",(req,res)=>{res.status(200).json({message:"conected"})})
-app.use("/api/auth",authRoute)
-app.use("/api/user",userRoute)
-app.use("/api/task",taskRoute)
+  // database
 
-
-// database
-
-const connectToDB = async () => {
+  const connectToDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
-};
+  };
 
-mongoose.connection.on("disconnected",()=>{
-    console.log("mongoDB disconnected!")
-});
-mongoose.connection.on("connected",()=>{ 
-    console.log("mongoDB connected!")
-});
+  mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected!");
+  });
 
- const server =  app.listen(port, () => {
-  connectToDB();
-  console.log(`app listning on http://localhost:${port}`);
-});
-
+  const server = app.listen(port, () => {
+    connectToDB();
+    console.log(`app listning on http://localhost:${port}`);
+  });
+}
 
 //* unhandled promise rejection - mongoConnection String error handling
-process.on('unhandledRejection',(err)=>{
-  console.log(`Error: ${err.message}`) 
-  console.log(`Shutting down the server due to inhandled promise rejection`)
-  server.close(()=>{
-     process.exit(1)
-  }) 
- })   
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to inhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
